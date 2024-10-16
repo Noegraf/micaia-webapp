aceb47bd3fa151834ca9e0e8725efd8182a9aa40
Noe Valle <nouvalle.graf@gmail.com>
2024-10-15 21:11:50 -0300

    prueba con dos pantallas

--- micaia/src/app/app.routes.ts
+++ micaia/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './src/components/welcome/welcome.component';
+import { CrearCuenta1Component } from './src/components/crear-cuenta1/crear-cuenta1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+];
+
+
